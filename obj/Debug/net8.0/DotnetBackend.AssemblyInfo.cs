be9018bdc1d09f905151629023816443e6a84f62
Caiuã Mello <105950039+caiua0800@users.noreply.github.com>
2024-11-29 18:58:37 -0300

    Banco de Dados Mongo DB Atlas rodando na cloud da aws 2

--- obj/Debug/net8.0/DotnetBackend.AssemblyInfo.cs
+++ obj/Debug/net8.0/DotnetBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DotnetBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1230ac057f207cb162d7fc9b9da54cbdce8a38c3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3f4d792205cdfad3fbec0031fa1109a57ac4de5")]
 [assembly: System.Reflection.AssemblyProductAttribute("DotnetBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DotnetBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
