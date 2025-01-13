2686c31119704a55140a9ecf6192d7fe04e4bb1d
Caiuã Mello <105950039+caiua0800@users.noreply.github.com>
2025-01-13 00:42:19 -0300

    Subindo daquele jeitinho depois de um tempo pra garantir mais mil conto no bolso do teu pai 2.0

--- obj/Debug/net8.0/DotnetBackend.AssemblyInfo.cs
+++ obj/Debug/net8.0/DotnetBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DotnetBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae4606972b4c2f6ea1972c6cb828e167b66c24e3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc2f33860327a346653ac897b1459663bafd8a93")]
 [assembly: System.Reflection.AssemblyProductAttribute("DotnetBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DotnetBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
