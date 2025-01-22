f16ecfd17aad3041cea83ed1d11539fed4b83665
Caiuã Mello <105950039+caiua0800@users.noreply.github.com>
2025-01-21 21:15:50 -0300

    mudei o dbs

--- obj/Debug/net8.0/DotnetBackend.AssemblyInfo.cs
+++ obj/Debug/net8.0/DotnetBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DotnetBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6ad04c26cdbf33462e60279a01253752ab364c3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2166eae371902dc5519d1ec239e9367088b64844")]
 [assembly: System.Reflection.AssemblyProductAttribute("DotnetBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DotnetBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
